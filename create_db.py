e46d3970d38fa0e5603cd3c45504c382be8ba4cf
Aaron Kyle <amityelmwood@gmail.com>
2022-11-08 20:01:44 +0800

    SIS project

---
+++ create_db.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost", user="root", passwd="waifu122")
+
+my_cursor = mydb.cursor()
+
+#my_cursor.execute("CREATE DATABASE SIS5")
+
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
